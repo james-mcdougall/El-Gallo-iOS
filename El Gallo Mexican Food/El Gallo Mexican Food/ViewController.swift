a2b501cde1363dd5f1fe8f56283e90f51937ba63
James McDougall <jameswiliammcdougall@gmail.com>
2020-07-17 17:32:39 -0500

    Initial Project Commit

---
+++ El Gallo Mexican Food/El Gallo Mexican Food/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  El Gallo Mexican Food
+//
+//  Created by Lambda_School_loaner_226 on 7/17/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
